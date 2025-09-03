7abe1a2a2f8627bdebe04608c1e435bd17db549a
romanshiaolin <148541408+romanshiaolin@users.noreply.github.com>
2025-09-03 14:28:26 +0000

    更動1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🔧 Tool Analysis Platform</h1>
+      <p>上傳 Amazon 頁面 PDF 或輸入連結，即可分析產品。</p>
+    </div>
+  );
+}
+
+export default App;
